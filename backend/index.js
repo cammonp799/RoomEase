7e74cc150ba8459397a0b5040d73c97ad090508d
Madjinda Lucas <lucasmadjinda@gmail.com>
2025-08-23 20:44:28 +0200

    MBA-32 initialiser serveur express

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');  
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Autorise les requêtes venant de ton front Angular
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Health check
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
